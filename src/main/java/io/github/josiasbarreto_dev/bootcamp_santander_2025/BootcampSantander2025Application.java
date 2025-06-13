34723dbaaa23dcd0b30c9e2029004d8b423addf0
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-06-13 10:09:31 -0300

    feat: first commit

---
+++ src/main/java/io/github/josiasbarreto_dev/bootcamp_santander_2025/BootcampSantander2025Application.java
@@ -0,0 +1,13 @@
+package io.github.josiasbarreto_dev.bootcamp_santander_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSantander2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantander2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/josiasbarreto_dev/bootcamp_santander_2025/BootcampSantander2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.josiasbarreto_dev.bootcamp_santander_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantander2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
